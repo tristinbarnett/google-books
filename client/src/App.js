c559c2b9479c33bf464639034139eee2bf767ecc
Tristin Barnett <tristinbarnett@gmail.com>
2020-06-27 10:58:22 -0700

    changed from components to pages for saved/search

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import Books from "./pages/Search";
+import Detail from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { BrowserRouter } from "react-router-dom";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
     <BrowserRouter>
